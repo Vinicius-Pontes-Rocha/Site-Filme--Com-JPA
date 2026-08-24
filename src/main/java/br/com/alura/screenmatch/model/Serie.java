106d7262aa357f2b77787874c2d3118c7b1e78d0
Vinicius-Pontes-Rocha <viniciuspontesrocha32@gmail.com>
2026-08-24 15:28:00 -0300

    JPA

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,17 +1,41 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaGemini;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(unique = true)
         private String titulo;
+
         private Integer totalTemporadas;
+
         private Double avaliacao;
+
+        @Enumerated(EnumType.STRING)
         private Categoria genero;
+
         private String atores;
+
         private String poster;
+
         private String sinopse;
+
+        @Transient
+        private List<Episodio> episodios = new ArrayList<>();
+
+    public Serie() {}
+
+
 
     public Serie(DadosSerie dadosSerie){
         this.titulo = dadosSerie.titulo();
@@ -78,6 +102,12 @@ public class Serie {
     public void setSinopse(String sinopse) {
         this.sinopse = sinopse;
     }
+
+    public List<Episodio> getEpisodios() {return episodios;}
+
+    public void setEpisodios(List<Episodio> episodios) {this.episodios = episodios;}
+
+
     @Override
     public String toString() {
         return "Serie{" +
